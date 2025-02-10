17018988fe406a7b21dd028b7f08a16229afdf48
Emek Taydaş <mithgroth@gmail.com>
2025-02-10 18:52:35 +0300

    longest-substring-without-repeating-characters

---
+++ medium/longest-substring-without-repeating-characters/src/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace src;
+
+public static class Solution
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        // Constraint: 0 <= s.length <= 5 * 10^4
+        // Which means, we should never try to load the entire string into memory
+
+        var max = 0;
+        var span = s.AsSpan();
+        var start = 0;
+
+        while (start < span.Length)
+        {
+            var tempMax = 0;
+            char[] current = [];
+            var subSpan = span[start..];
+
+            for (var i = 0; i < subSpan.Length; i++)
+            {
+                if (Array.IndexOf(current, subSpan[i]) != -1)
+                {
+                    break;
+                }
+
+                tempMax++;
+                current = [.. current, subSpan[i]];
+            }
+
+            max = Math.Max(max, tempMax);
+            start++;
+        }
+
+        return max;
+    }
+}
---
+++ medium/longest-substring-without-repeating-characters/test/Test.cs
@@ -0,0 +1,35 @@
+﻿using src;
+
+namespace test;
+
+public class Test
+{
+    public static IEnumerable<object[]> GetExamples()
+    {
+        yield return new object[]
+        {
+            "abcabcbb",
+            3,
+        };
+
+        yield return new object[]
+        {
+            "bbbbb",
+            1,
+        };
+
+        yield return new object[]
+        {
+            "pwwkew",
+            3,
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetExamples))]
+    public void Test1(string s, int expected)
+    {
+        var actual = Solution.LengthOfLongestSubstring(s);
+        Assert.Equivalent(actual, expected);
+    }
+}
